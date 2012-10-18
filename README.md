979a203e2a7229b4570fbc2a00006f57bafaece8
Karel Piwko <kpiwko@redhat.com>
2012-10-18 16:21:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+EL Profile Activation Maven Extension
+-------------------------------------
+
+Allows profile to be activated using an expression language expression. Currently supports MVEL2 only. 
+Extension hijacks property activation and tries to evaluate mvel expression first, if this is not successful
+it passes control to original property activator.
+
+In order to activate extension, you cannot include it into <build><extensions>, because profile activation is done
+before it would be activated. So you need to copy following files into *$MAVEN_HOME/lib/ext*:
+
+* el-profile-activator-extension.jar (available in target directory)
+* mvel2-${version} (available http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22org.mvel%22%20AND%20a%3A%22mvel2%22, substitute with latest version)  
+
+To profit from new activation, add following in your pom.xml:
+
+    <profile>
+        <id>my-profile</id>
+    
+        <activation>
+            <property>
+            	<!-- mvel property name is obligatory -->
+                <name>mvel</name>
+                <value>isdef foo &amp;&amp; foo=="abc"</value>
+            </property>
+        </activation>
+    </profile>            
+    
+A few examples (an MVEL cheatsheet)
+-----------------------------------
+
+* Check if *foo* and *bar* are defined and have same value 
+ 
+		isdef foo &amp;&amp; isdef bar &amp;&amp; foo==bar
+		
+* Check if *foo* is defined while *bar* is not
+		
+	    isdef foo &amp;&amp; !isdef bar
+	    
+* Check if *foo* starts with *abc* or *baz* contains *xyz*
+	
+		isdef foo &amp;&amp; foo.startsWith("abc")) || (isdef baz &amp;&amp; baz.contains("xyz"))
+
+Complete MVEL reference guide is available at http://mvel.codehaus.org/Language+Guide+for+2.0		
---
+++ src/main/java/com/redhat/jboss/maven/elprofile/ElProfileActivator.java
@@ -0,0 +1,100 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.redhat.jboss.maven.elprofile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.maven.model.Activation;
+import org.apache.maven.model.ActivationProperty;
+import org.apache.maven.model.Profile;
+import org.apache.maven.model.building.ModelProblemCollector;
+import org.apache.maven.model.profile.ProfileActivationContext;
+import org.apache.maven.model.profile.activation.ProfileActivator;
+import org.apache.maven.model.profile.activation.PropertyProfileActivator;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.Logger;
+import org.mvel2.CompileException;
+import org.mvel2.MVEL;
+
+/**
+ * Alternative implementation of PropertyActivator for Maven 3
+ * 
+ * @author <a href="mailto:kpiwko@redhat.com">Karel Piwko</a>
+ * 
+ */
+@Component(role = ProfileActivator.class, hint = "property")
+public class ElProfileActivator implements ProfileActivator {
+
+    private static final String MVEL_SCRIPT_PROPERTY_NAME = "mvel";
+
+    @Requirement
+    private Logger logger;
+
+    public boolean isActive(Profile profile, ProfileActivationContext context, ModelProblemCollector problemCollector) {
+
+        Activation activation = profile.getActivation();
+
+        boolean result = false;
+
+        if (activation != null)
+        {
+            ActivationProperty property = activation.getProperty();
+
+            if (property != null)
+            {
+                String name = property.getName();
+
+                if (name != null && MVEL_SCRIPT_PROPERTY_NAME.equals(name)) {
+                    String value = property.getValue();
+                    logger.debug("Evaluating following MVEL expression: " + value);
+                    result = evaluateMvel(value, context, problemCollector);
+                    logger.debug("Evaluated MVEL expression: " + value + " as " + result);
+                }
+            }
+        }
+
+        // call original implementation if mvel script was not valid/false
+        return result ? true : new PropertyProfileActivator().isActive(profile, context, problemCollector);
+    }
+
+    private boolean evaluateMvel(String expression, ProfileActivationContext context, ModelProblemCollector problemCollector) {
+
+        if (expression == null || expression.length() == 0) {
+            return false;
+        }
+
+        try {
+            // "casting" to <String,Object> and including both user and system properties
+            Map<String, Object> externalVariables = new HashMap<String, Object>();
+            externalVariables.putAll(context.getSystemProperties());
+            externalVariables.putAll(context.getUserProperties());
+
+            return MVEL.evalToBoolean(expression, externalVariables);
+        } catch (NullPointerException e) {
+            logger.warn("Unable to evaluate mvel property value (\"" + expression + "\")");
+            logger.debug(e.getMessage());
+            return false;
+        } catch (CompileException e) {
+            logger.warn("Unable to evaluate mvel property value (\"" + expression + "\")");
+            logger.debug(e.getMessage());
+            return false;
+        }
+    }
+
+}
